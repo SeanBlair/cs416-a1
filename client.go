8008c049dfc8dfc01891ea051e81212b66fd812b
Sean Blair <seandibango@gmail.com>
2017-01-08 14:48:14 -0800

    first commit

---
+++ client.go
@@ -0,0 +1,42 @@
+/*
+Implements the solution to assignment 1 for UBC CS 416 2016 W2.
+
+Usage:
+$ go run client.go [local UDP ip:port] [server UDP ip:port]
+
+Example:
+$ go run client.go 127.0.0.1:2020 127.0.0.1:7070
+
+*/
+
+package main
+
+import (
+	"encoding/gob"
+	"fmt"
+	"os"
+)
+
+// Main workhorse method.
+func main() {
+	args := os.Args[1:]
+
+	// Missing command line args.
+	if len(args) != 2 {
+		fmt.Println("Usage: client.go [local UDP ip:port] [server UDP ip:port]")
+		return
+	}
+
+	// Extract the command line args.
+	local_ip_port := args[0]
+	remote_ip_port := args[1]
+
+	// TODO
+}
+
+func Marshall(guess uint32) ([]byte, error) {
+	var network bytes.Buffer
+	enc := gob.NewEncoder(&network)
+	err := enc.Encode(guess)
+	return network.Bytes(), err
+}
